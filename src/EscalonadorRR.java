12e29caad78814a50a93a545ab5957bf6cde4e43
Felipe Silveira <felipe.silveira@edu.pucrs.com>
2023-04-15 17:52:27 -0300

    começando o round-robin

---
+++ src/EscalonadorRR.java
@@ -0,0 +1,25 @@
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class EscalonadorRR extends Escalonador {
+  private int time;
+  private LinkedList<Processo> blockedQueue;
+  private LinkedList<Processo> readyQueue;
+  private LinkedList<Processo> notStartedQueue;
+  private Processo runningProcess;
+  private int quantum;
+
+  public EscalonadorRR(LinkedList<Processo> processes, int quantum) {
+    this.readyQueue = new LinkedList<>();
+    this.notStartedQueue = processes;
+    this.runningProcess = null;
+    this.quantum = quantum;
+  }
+
+  @Override
+  int run() {
+    // implementar lógica round-robin
+  }
+}
