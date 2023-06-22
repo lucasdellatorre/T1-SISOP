7d3f67a545f6ed5e115447d9d950060d327e160d
lucasdellatorre <lucasdellatorre222@gmail.com>
2023-06-22 13:23:10 -0300

    add menu buddy

--- t2/Buddy.java
+++ t2/Buddy.java
@@ -1,53 +1,31 @@
-public class Buddy {
-    private MemoryBuddy root;
+import java.util.ArrayList;
 
-    public Buddy(int totalSize) {
-        root = new MemoryBuddy(totalSize);
+public class Buddy{
+    private ArrayList<Request> requests;
+    private BuddyMem memory;
+    
+    public Buddy(BuddyMem memory, ArrayList<Request> requests) {
+        this.memory = memory;
+        this.requests = requests;
     }
 
-    public int allocate(int size) {
-        if (!Util.isPowerOfTwo(size) || size > root.getSize() || root.isAllocated()) {
-            System.out.println("Invalid allocation size");
-            return -1; // Invalid allocation size or memory is fully allocated
+    public void execute() {
+        try {
+            for (Request request : requests) {
+                if (request.getCommand() == "IN") {
+                    memory.insert(request.getPSize(), request.getPid());
+                } else if (request.getCommand() == "OUT") {
+                    memory.remove(request.getPid());
+                } else {
+                    System.err.println("Buddy: command name error");
+                }
+                memory.printMemoryState();
+            }
+        } catch (InsufficientMemoryException ime) {
+            System.err.println(ime.getMessage());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
         }
 
-        MemoryBuddy block = findFreeBlock(root, size);
-        if (block == null) {
-            return -1; // No suitable block found
-        }
-
-        splitBlock(block, size);
-        block.allocate();
-        return block.getSize();
-    }
-
-    private MemoryBuddy findFreeBlock(MemoryBuddy node, int size) {
-        if (node.getSize() == size && !node.isAllocated()) {
-            return node; // Exact match found
-        }
-
-        if (node.getSize() < size || node.isAllocated()) {
-            return null; // No suitable block found
-        }
-
-        MemoryBuddy leftChild = node.getLeftChild();
-        MemoryBuddy rightChild = node.getRightChild();
-
-        MemoryBuddy block = findFreeBlock(leftChild, size);
-        return block != null ? block : findFreeBlock(rightChild, size);
-    }
-
-    private void splitBlock(MemoryBuddy block, int size) {
-        while (block.getSize() > size * 2) {
-            int newSize = block.getSize() / 2;
-
-            MemoryBuddy leftChild = new MemoryBuddy(newSize);
-            MemoryBuddy rightChild = new MemoryBuddy(newSize);
-
-            block.setLeftChild(leftChild);
-            block.setRightChild(rightChild);
-
-            block = leftChild;
-        }
     }
 }
